d41dad47959a3753f91b1aa896ff71259295d164
Gohan <yohanencarnacion2@gmail.com>
2026-05-10 13:15:40 -0400

    change were made to the following files : to FoodCard.tsx card recieve the correct data pass down from the types/ food file, same with the FoodCardList.tsx but a show more button was created. while the for the itemDetail.tsx file recieve the correct prop data.

---
+++ food-store/src/types/food.ts
@@ -0,0 +1,15 @@
+export interface FoodItem {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+}
+
+export interface ItemDetailProps {
+  foodItem: FoodItem;
+}
+
+export interface FoodCardListProps {
+  foodItems: FoodItem[];
+}
